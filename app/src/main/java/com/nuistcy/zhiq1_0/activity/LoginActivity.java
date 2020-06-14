0a97a8d71a97f061b12a5d405bdb712d51c81616
KIKOcaoyue <304960005@qq.com>
2020-06-14 20:01:38 +0800

    access data from bmob and anonymous inner class and sub thread problems solved. login successed.

--- app/src/main/java/com/nuistcy/zhiq1_0/activity/LoginActivity.java
+++ app/src/main/java/com/nuistcy/zhiq1_0/activity/LoginActivity.java
@@ -8,6 +8,8 @@ import cn.bmob.v3.listener.FindListener;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -19,64 +21,92 @@ import com.nuistcy.zhiq1_0.R;
 import com.nuistcy.zhiq1_0.entity.User;
 import com.nuistcy.zhiq1_0.nets.socketHandle;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
-
-    private socketHandle sockethandle = null;
-    private List<User> userlist = null;
+    private static final int QUERYDATABASE = 1;
+    private String bmobresult;
+    private static TextView editusername;
+    private static TextView editpwd;
+    private static Button btnlogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Button btnlogin = (Button) findViewById(R.id.btnlogin);
-        Bmob.initialize(this,"mykey");
+        btnlogin = (Button) findViewById(R.id.btnlogin);
+        Bmob.initialize(this,"b9194c9e79bfa98be319126e045f09d1");
+        editusername = (TextView) findViewById(R.id.editusername);
+        editpwd = (TextView) findViewById(R.id.editpwd);
         btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                TextView editusername = (TextView) findViewById(R.id.editusername);
-                TextView editpwd = (TextView) findViewById(R.id.editpwd);
-                //String loginrequest = loginUtils.generateLoginRequest(editusername.getText().toString(),editpwd.getText().toString());
-//                try{
-//                    sockethandle.send(loginrequest);
-//                }catch(Exception e) {
-//                    Toast.makeText(LoginActivity.this,"发送失败",Toast.LENGTH_LONG).show();
-//                    e.printStackTrace();
-//                }
-                boolean canlogin = checklogin(editusername.getText().toString().trim(),editpwd.getText().toString().trim());
-                if(canlogin==true){
-                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                }else{
-                    Toast.makeText(LoginActivity.this,"无法登录",Toast.LENGTH_LONG).show();
-                }
+                checklogin(editusername.getText().toString().trim(),editpwd.getText().toString().trim());
             }
         });
     }
 
-    public boolean checklogin(String userid,String pwd){
+    private void checklogin(String userid,String pwd){
         BmobQuery<User> userquery = new BmobQuery<User>();
         userquery.addWhereEqualTo("userid",userid);
         userquery.findObjects(new FindListener<User>() {
             @Override
             public void done(List<User> list, BmobException e) {
                 if(e==null){
-                    LoginActivity.this.userlist = list;
-                    Log.d("BMOB",LoginActivity.this.userlist.get(0).getUsername());
+                    Message msg = Message.obtain();
+                    msg.what = QUERYDATABASE;
+                    msg.obj = list.get(0).getUserpwd()+"#"+pwd; //a#b
+                    handler.sendMessage(msg);
                 }else{
-                    Log.d("BMOB","NULL");
+                    Message msg = Message.obtain();
+                    msg.what = QUERYDATABASE;
+                    msg.obj = "NULL"; //a#b
+                    handler.sendMessage(msg);
                 }
             }
         });
-        Log.d("BMOB",LoginActivity.this.userlist.get(0).getUsername());
-        if(LoginActivity.this.userlist==null){
-            return false;
-        }else{
-            if(LoginActivity.this.userlist.get(0).getUserpwd()==pwd){
-                return true;
-            }else{
-                return false;
+    }
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch(msg.what){
+                case QUERYDATABASE:
+                    bmobresult = (String) msg.obj;
+                    if(loginsuccess(bmobresult)==true){
+                        startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                    }else{
+                        Toast.makeText(LoginActivity.this,"无法登录",Toast.LENGTH_LONG).show();
+                    }
+                    break;
+                default:
+                    break;
             }
+        }
+    };
+
+    private boolean loginsuccess(String request){
+        String correctpwd = "";
+        String inputpwd = "";
+        boolean found = false;
+        for(int i=0;i<request.length();i++){
+            if(request.charAt(i)=='#'){
+                found = true;
+                continue;
+            }
+            if(!found) {
+                correctpwd += request.charAt(i);
+            }
+            if(found){
+                inputpwd += request.charAt(i);
+            }
+        }
+        if(correctpwd.equals(inputpwd)){
+            return true;
+        }else{
+            return false;
         }
     }
 }
